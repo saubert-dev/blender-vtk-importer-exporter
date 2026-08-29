f3c20ecd92180cfd3c5e2a3e96500348fd733b13
Stéphane AUBERT <stephane.aubert67@gmail.com>
2026-08-29 10:06:15 +0200

    Simplification of the reference to the dataset if the tested fixture is modified

--- tests/test_attributes_initialize_material_attributes.py
+++ tests/test_attributes_initialize_material_attributes.py
@@ -79,9 +79,11 @@ class TestClass:
         t_domain, b_domain, suffix, # Domain of the attribute
         name, data_type # Type of data stored in attribute 
     ):
+        vtk_data = pvUG_one_triangle
+        
         # Mesh and Material setup
         mesh_name = unique_mesh_name()
-        mesh = m_mesh.vtk_to_mesh(pvUG_one_triangle, mesh_name)
+        mesh = m_mesh.vtk_to_mesh(vtk_data, mesh_name)
         mat = bpy.data.materials.new(name=f"{mesh_name}_attributes")
         mat["attributes"] = {}
         
@@ -89,7 +91,7 @@ class TestClass:
         t_name = name
         if name not in ("Texture Coordinates", "Normals"):
             t_name += suffix
-        t_data = getattr(pvUG_one_triangle, t_domain)
+        t_data = getattr(vtk_data, t_domain)
         if t_data.get(t_name) is None:
             pytest.skip("Irrelevant attribute")
         t_values = t_data[t_name]
