c7ce1c6b71be994307a3117916a3b9a9c35d2822
Stéphane AUBERT <stephane.aubert67@gmail.com>
2026-09-12 15:02:34 +0200

    Parametrization of the type of PyVista DataSet for the test_one_triangle() function

--- tests/test_mesh_vtk_to_mesh.py
+++ tests/test_mesh_vtk_to_mesh.py
@@ -2,6 +2,8 @@
 
 import bpy
 
+import pytest
+
 from utilities import *
 
 
@@ -19,11 +21,28 @@ class TestClass:
         assert bpy.data.meshes.find(mesh_name) != -1 # "find != -1" means "found"
         
 
-    def test_one_triangle(self, PolyData_one_triangle):
+    # Parametrization of the type of PyVista DataSet
+    @pytest.mark.parametrize("dataset_type", ["PolyData", "UnstructuredGrid"])
+    def test_one_triangle(
+        self,
+        dataset_type,
+        PolyData_one_triangle
+    ):
+        dataset = PolyData_one_triangle
+        match dataset_type:
+            case "PolyData":
+                vtk_data = dataset
+            case "UnstructuredGrid":
+                vtk_data = dataset.cast_to_unstructured_grid()
+            case _:
+                msg = f"Unsupported PyVista DataSet type: {dataset_type}."
+                raise ValueError(msg)
+        
         mesh = m_mesh.vtk_to_mesh(
-            PolyData_one_triangle,
+            vtk_data,
             unique_mesh_name()
         )
+        
         assert len(mesh.vertices) == 3
         assert len(mesh.edges)    == 3
         assert len(mesh.polygons) == 1
